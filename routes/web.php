46752b250db215eb1947a039f0740501dee83163
theaf1 <sophon.nit@mahidol.ac.th>
2019-06-27 10:34:33 +0700

    live learn 27062562

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',function() {
+    return "hello world";
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/tasks',function(){
+    return "Hello World";
+})->middleware('auth');
