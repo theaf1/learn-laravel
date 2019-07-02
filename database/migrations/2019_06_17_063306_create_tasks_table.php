8da6e20f6c570bc32bd649539d53946e6938675a
theaf1 <sophon.nit@mahidol.ac.th>
2019-07-02 13:53:32 +0700

    episode4

--- database/migrations/2019_06_17_063306_create_tasks_table.php
+++ database/migrations/2019_06_17_063306_create_tasks_table.php
@@ -17,6 +17,8 @@ class CreateTasksTable extends Migration
             $table->bigIncrements('id');
             $table->integer('type_id');
             $table->string('name');
+            $table->string('detail');
+            $table->boolean('status');
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
